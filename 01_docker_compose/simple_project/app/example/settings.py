46159ac50b1330b4e04907f38e79afd50d1fe757
zamula-soft <ivan.zamula@gmail.com>
2022-06-24 03:50:19 +0300

    initial commit

--- 01_docker_compose/simple_project/app/example/settings.py
+++ 01_docker_compose/simple_project/app/example/settings.py
@@ -9,7 +9,6 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -91,7 +90,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
